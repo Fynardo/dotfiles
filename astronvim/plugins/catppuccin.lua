70d79906150cf817a78ba64d55479392271c8f0e
Diego Noceda <dnoceda@redhat.com>
2025-08-01 12:06:07 +0200

    Updated astronvim config to v5.0

--- astronvim/plugins/catppuccin.lua
+++ astronvim/plugins/catppuccin.lua
@@ -1,22 +1,12 @@
 return {
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     config = function()
       require("catppuccin").setup({
-        flavour = "frappe",
         background = {
           light="latte",
           dark="frappe"
         }
       })
     end,
-  },
-  {
-    "neanias/everforest-nvim",
-    name = "everforest-nvim",
-    config = function()
-      require("everforest").setup {}
-    end,
-  },
 }
